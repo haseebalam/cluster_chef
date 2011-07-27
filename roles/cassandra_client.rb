4fef18b51e71f118be71457f2223de6052abe04a
Mike Heffner <mikeh@fesnel.com>
2011-07-26 23:24:10 -0400

    Remove role defaults already set in the recipe attributes.

--- roles/cassandra_client.rb
+++ roles/cassandra_client.rb
@@ -16,10 +16,6 @@ run_list *%w[
 default_attributes({
     :cassandra => {
       :auto_bootstrap    => true,
-      :jmx_port          => 12345,
-      :concurrent_reads  => 4,
-      :concurrent_writes => 64,
-      :commitlog_sync    => 'periodic',
       :data_file_dirs    => ["/data/db/cassandra"],
     }
   })
