b313be4496b287d9b61fe9975731872362b20679
Mike Heffner <mikeh@fesnel.com>
2011-08-16 09:54:46 -0400

    Update/fix the ec2snitch support.

--- site-cookbooks/cassandra/recipes/ec2snitch.rb
+++ site-cookbooks/cassandra/recipes/ec2snitch.rb
@@ -22,20 +22,21 @@
 # to org.apache.cassandra.locator.PropertyFileEndPointSnitch.  See contrib/prop
 # in the cassandra distribution for details.
 
-@topo_map = search(:node, "cassandra_cluster_name:#{node[:cassandra][:cluster_name]} AND ec2:placement_availability_zone").map do |n|
+topo_map = {}
+search(:node, "cassandra_cluster_name:#{node[:cassandra][:cluster_name]} AND ec2:placement_availability_zone").map do |n|
   az_parts = n[:ec2][:placement_availability_zone].split('-')
-  @topo_map[n['ipaddress']] = {:port => n[:cassandra][:storage_port],
-                               :rack => az_parts.last[1],
-                               :dc => az_parts[0..1].join('-') + "-#{az_parts.last[0]}"}
+  topo_map[n['ipaddress']] = {
+    :rack => az_parts.last[1, 1],
+    :dc => az_parts[0..1].join('-') + "-#{az_parts.last[0, 1]}"}
 end
 
 node.set_unless[:cassandra][:ec2_snitch_default_az] = "us-east-1a"
 default_az_parts = node[:cassandra][:ec2_snitch_default_az].split('-')
-@default_az = {:rack => default_az_parts.last[1], :dc => default_az_parts[0..1].join('-') + "-#{default_az_parts.last[0]}"}
+default_az = {:rack => default_az_parts.last[1, 1], :dc => default_az_parts[0..1].join('-') + "-#{default_az_parts.last[0, 1]}"}
 
-template "/etc/cassandra/rack.properties" do
-  variables(:topo_map => @topo_map, :default_az => @default_az)
-  source "rack.properties.erb"
+template "/etc/cassandra/cassandra-topology.properties" do
+  variables(:topo_map => topo_map, :default_az => default_az)
+  source "cassandra-topology.properties.erb"
   owner "root"
   group "root"
   mode 0644
